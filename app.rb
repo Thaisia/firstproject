f5d01b9c332753bd75d5b14c775aaa57cf278161
Taisia Bo <thaisbox@gmail.com>
2016-08-22 16:18:23 +0300

    modifying content and content uploading

--- app.rb
+++ app.rb
@@ -12,11 +12,12 @@ end
 
 before do
   db = get_db
-  @main = db.execute 'select * from main;'
-  @faces = db.execute 'select * from faces;'
-	@places = db.execute 'select * from places;'
-  @events = db.execute 'select * from events;'
-  @closeup = db.execute 'select * from closeup;'
+  @main = db.execute 'select * from pho;'
+  @faces = db.execute 'select pic_link, theme_name from pho join themes on pho.theme_id = themes.id where themes.parent_id = 1 or themes.parent_id in (select id from themes where parent_id=1);'
+	@places = db.execute 'select pic_link, theme_name from pho join themes on pho.theme_id = themes.id where themes.parent_id = 2 or themes.parent_id in (select id from themes where parent_id=2);'
+  @events = db.execute 'select pic_link, theme_name from pho join themes on pho.theme_id = themes.id where themes.parent_id = 3 or themes.parent_id in (select id from themes where parent_id=3);'
+  @closeup = db.execute 'select pic_link, theme_name from pho join themes on pho.theme_id = themes.id where themes.parent_id = 4 or themes.parent_id in (select id from themes where parent_id=4);'
+
 end
 
 get '/' do
@@ -24,25 +25,29 @@ get '/' do
 end
 
 get '/faces' do
-	erb :faces, :layout => false
+  if params["ajax"] == "1"
+    erb :faces, :layout => false
+  else
+    erb :faces
+  end
 end
 
 get '/places' do
-	erb :places
+	erb :places, :layout => false
 end
 
 get '/events' do
-	erb :events
+	erb :events, :layout => false
 end
 
 get '/closeup' do
-	erb :closeup
+	erb :closeup, :layout => false
 end
 
 get '/coaching' do
-	erb :coaching
+	erb :coaching, :layout => false
 end
 
 get '/aboutme' do
-	erb :aboutme
+	erb :aboutme, :layout => false
 end
