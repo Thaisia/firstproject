3b360ef84a891f173d8b59f9e766f1acc7a86e4e
Taisia Bo <thaisbox@gmail.com>
2016-08-24 02:12:21 +0300

    adding tags and content for tags

--- app.rb
+++ app.rb
@@ -13,6 +13,8 @@ end
 before do
   db = get_db
   @themes = db.execute 'select * from themes;'
+  @tags = db.execute 'select * from tags'
+  @pho_tags = db.execute 'select pic_link from pho join pho_tags on pho.id = pho_tags.photos_id where tags_id  = 19'
 end
 
 
@@ -44,8 +46,24 @@ get '/gallery/:url_part' do
     @theme_id = db.execute 'select id from themes where theme_link=?', params[:url_part]
     theme_id = @theme_id[0]['id']
 
-    @photos = db.execute 'select pic_link from pho join themes on pho.theme_id = themes.id where theme_id = ? or themes.parent_id = ? or themes.parent_id in (select id from themes where parent_id=?);', theme_id, theme_id, theme_id
+    @photos = db.execute 'select pic_link from pho join themes on pho.theme_id = themes.id where theme_id = ? or themes.parent_id = ? or themes.parent_id in (select id from themes where parent_id=?);',
+     theme_id, theme_id, theme_id
     if params["ajax"] == "1"
+      erb :gallery, :layout => false
+    else
+      erb :gallery
+    end
+end
+
+get '/tags/:url_part' do
+    db = get_db
+
+    @tags_id = db.execute 'select id from tags where tag_name=?', params[:url_part]
+    tag_id = @tags_id[0]['id']
+
+    @photos = db.execute 'select pic_link from pho join pho_tags on pho.id = pho_tags.photos_id where tags_id  = ?', tag_id
+    if params["ajax"] == "1"
+      
       erb :gallery, :layout => false
     else
       erb :gallery
