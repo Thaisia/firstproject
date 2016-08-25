a922657605a11671a8616e5c31e262e1b07e70b1
Taisia Bo <thaisbox@gmail.com>
2016-08-25 22:36:22 +0300

    refactoring

--- app.rb
+++ app.rb
@@ -24,6 +24,17 @@ get '/' do
 	erb :main
 end
 
+
+
+get '/blog' do
+  if params["ajax"] == "1"
+	  erb :blog, :layout => false
+  else
+    erb :blog
+  end
+end
+
+
 get '/coaching' do
   if params["ajax"] == "1"
 	  erb :coaching, :layout => false
@@ -40,7 +51,7 @@ get '/aboutme' do
   end
 end
 
-get '/gallery/:url_part' do
+get '/themes/:url_part' do
     db = get_db
 
     @theme_id = db.execute 'select id from themes where theme_link=?', params[:url_part]
@@ -48,6 +59,7 @@ get '/gallery/:url_part' do
 
     @photos = db.execute 'select pic_link from pho join themes on pho.theme_id = themes.id where theme_id = ? or themes.parent_id = ? or themes.parent_id in (select id from themes where parent_id=?);',
      theme_id, theme_id, theme_id
+
     if params["ajax"] == "1"
       erb :gallery, :layout => false
     else
@@ -63,7 +75,7 @@ get '/tags/:url_part' do
 
     @photos = db.execute 'select pic_link from pho join pho_tags on pho.id = pho_tags.photos_id where tags_id  = ?', tag_id
     if params["ajax"] == "1"
-      
+
       erb :gallery, :layout => false
     else
       erb :gallery
