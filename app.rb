7d8e889e3db044d5782ecc3494646d672a01672b
Taisia Bo <thaisbox@gmail.com>
2016-08-29 13:43:29 +0300

    loading and displaying changes

--- app.rb
+++ app.rb
@@ -19,7 +19,8 @@ end
 
 get '/' do
   db = get_db
-  @main = db.execute 'select * from pho;'
+    @photos = db.execute 'select pic_link from pho;'
+    @counter = 1
 	erb :main
 end
 
@@ -66,6 +67,7 @@ end
 
 get '/tags/:url_part' do
     db = get_db
+    @counter = 1
     @tags_id = db.execute 'select id from tags where tag_name=?', params[:url_part]
     tag_id = @tags_id[0]['id']
     @photos = db.execute 'select pic_link from pho join pho_tags on pho.id = pho_tags.photos_id where tags_id  = ?', tag_id
@@ -76,16 +78,9 @@ get '/tags/:url_part' do
     end
 end
 
-
-
-
-
-
-
-
 get '/themes/:url_part' do
     db = get_db
-
+@counter = 1
     @theme_id = db.execute 'select id from themes where theme_link=?', params[:url_part]
     theme_id = @theme_id[0]['id']
 
